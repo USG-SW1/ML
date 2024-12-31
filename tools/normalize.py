b680ead26a5426514b02120789cb5e737eb5137b
Max Chen <mchen@zyxel.com.tw>
2024-12-31 18:17:18 +0800

    Update normalize.py
    Add AI reviewer

--- tools/normalize.py
+++ tools/normalize.py
@@ -5,6 +5,7 @@ import json
 import pandas as pd
 import sys
 import datetime
+import ollama
 
 def epoch_to_iso8601(epoch_string):
   """將epoch字串轉換為ISO 8601格式
@@ -32,16 +33,70 @@ def custom_function(x):
     # 在這裡可以進行更複雜的計算或判斷
     return str_epoch
 
+def query_ai_qwen(features):
+    """
+    根據輸入的提示詞，返回兩個字串，第一個是嚴重性，第二個是AI的評論
+
+    Args:
+        prompt (str): 提示詞
+
+    Returns:
+        tuple: (severity, comment)
+    """
+    prompt = (
+        "Here is a http content. Analysis it and provide an attacked levels. "
+        "normal: it's a normal content. low: It is an abnormal content, but "
+        "it's not an attacked. medium: it's an attacked and just have a try. "
+        "high: it's an attacked, suggest admin verify the behavior "
+        "on http server. The answer format, Attack risk level: <normal or low or medium or high>. "
+        "And simple comment. No need any code."
+    )
+
+    #p= f"prompt + '\n' + features"
+    #print(prompt + '\n' + features)
+    #os.exit()
+    stream = ollama.chat(
+        model='qwen2.5-coder:32b',
+        messages=[{'role': 'user', 'content': prompt + '\n' + features}],
+        stream=True
+        )
+
+    # 取得AI的回應
+    severity = 'unknown'
+    comment = ''
+    for response in stream:
+        #print(response['message']['content'], end='', flush=True)
+        response_content = response['message']['content']
+        #if 'Attack risk level:' in response_content:
+        #    severity = response_content.split('Attack risk level:')[1].split('.')[0].strip()
+        comment = comment + response_content
+        #print(response_content)
+        print(response['message']['content'], end='', flush=True)
+        #print("aaaaaaa\n")
+    if 'Attack risk level:' in comment:
+       severity = comment.split('Attack risk level:')[1].split('\n')[0].strip()
+    print(comment)
+    print(severity)
+    # 模擬AI的回應
+    #comment = 'This is a simulated AI response.'
+    # 根據提示詞進行一些簡單的判斷
+
+    return severity, comment
+
 def process_json_files(input_directory, pass_path, pass_date):
     all_data = pd.DataFrame()
-    print("-"+input_directory+"-")
-    print("-"+pass_path+"-")
+    print("-" + input_directory + "-")
+    print("-" + pass_path + "-")
     output_file = "../raw-data/" + input_directory + "/" + pass_path
-    #for filename in os.listdir("../raw_data/" + pass_path):
+
     for filename in os.listdir("../raw-data/" + input_directory + "/" + pass_path):
         print(filename)
-        #sys.exit(1)
-        #if filename.endswith('.json'):
+        if filename[0] != 'S' or '-' not in filename:
+            sn = 'NULL'
+        else:
+            sn = filename.split('-')[0]
+        print("sn: " + sn)
+
         if filename.endswith('.json') and filename != os.path.basename(output_file):
             filepath = os.path.join(output_file, filename)
             with open(filepath) as f:
@@ -54,64 +109,32 @@ def process_json_files(input_directory, pass_path, pass_date):
             df['firmware'] = d['payload']['device']['firmware']
             df['model'] = d['payload']['device']['model']
             df['mode'] = d['payload']['device']['mode']
+
             if input_directory == 'webml':
-              # 將 feature_raw 資料依據 "=" 拆分成不同欄位並新增至 DataFrame 中
-              df['first_seen_time'] = df['first_seen_time'].astype(str)
-              df['first_seen_time'] = df['first_seen_time'].apply(custom_function)
-              for index, row in df.iterrows():
-                  features = row['feature_raw'].split('\n')
-                  for feature in features:
-                      if '=' in feature:
-                          key, value = feature.split('=', 1)
-                          df.at[index, key] = value
-                  #for index2, row2 in df.iterrows():
-                  #df['first_seen_time'] = df['first_seen_time'].astype(str)
-                  #str_epoch = f"{df['first_seen_time']}"
-                  #print("aaaa " + str_epoch)
-                  #print("bbb " + df['first_seen_time'])
-                  #df['first_seen_time'] = epoch_to_iso8601(str_epoch)
-                  #first_seen_times = row['first_seen_time'].split('\n')
-                  #print(first_seen_times)
-              #for index, row in df.iterrows():
-                  #first_seen_times = row['first_seen_time'].split('\n')
-                  #print(df[index,'first_seen_time'])
-                  #first_seen_times = row['first_seen_time']
-                  #for first_seen_time in first_seen_times:
-                  #    print("value: " + first_seen_time)
-                  #    print("ccc")
+                # 將 feature_raw 資料依據 "=" 拆分成不同欄位並新增至 DataFrame 中
+                df['first_seen_time'] = df['first_seen_time'].astype(str)
+                df['first_seen_time'] = df['first_seen_time'].apply(custom_function)
 
-                  #print("aaaa")
-                  #print(df['first_seen_time'])
-                  #str_epoch = f"{df['first_seen_time']}"
-                  #print(epoch_to_iso8601(str_epoch))
-                  #df['first_seen_time'] = df['first_seen_time'].astype(str)
-                  #df['first_seen_time'] = str_epoch
-                  #df['first_seen_time'] = "1970-01-02T03:11:40"
-                  #print(df['first_seen_time'])
-                  #epoch_to_iso8601(172000)
+                for index, row in df.iterrows():
+                    features = row['feature_raw'].split('\n')
+                    for feature in features:
+                        if '=' in feature:
+                            key, value = feature.split('=', 1)
+                            df.at[index, key] = value
 
-            # 將當前 DataFrame 合併到 all_data 中
+                for index, row in df.iterrows():
+                    features = row['feature_raw'].split('\n\n', 1)
+                    if len(features) > 1:
+                        feature_string = features[1]
+                        severity, comment = query_ai_qwen(feature_string)
+                        df.at[index, 'severity'] = severity
+                        df.at[index, 'ai_comment'] = None if comment == 'NULL' else comment
+
+            df['sn'] = None if sn == 'NULL' else sn
             all_data = pd.concat([all_data, df], ignore_index=True)
-
-
-    # 取得當前日期和時間
-    # current_date = datetime.now()
-    #current_date = datetime.now() - timedelta(days=1)
-
-    # 取得年、月、日
-    #year = current_date.year
-    #month = current_date.month
-    # 處理前一天資料
-    #day = current_date.day
-    #if day < 10:
-    #    output_filename = f"{str(year) + '-' + str(month) + '-' + '0' + str(day) + '_' + 'webml'}.json"
-    #else:
-    #    output_filename = f"{str(year) + '-' + str(month) + '-' + str(day) + '_' + 'webml'}.json"
 
     output_filename = f"{pass_date + '_' + 'webml'}.json"
     print(output_filename)
-    # 將合併後的 DataFrame 寫入 output.json 中
-    #all_data.to_json(output_filename, orient='records', lines=True)
     all_data.to_json(pass_date + "_" + input_directory + ".json", orient='records', lines=True)
 
 if __name__ == "__main__":
